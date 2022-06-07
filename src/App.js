2595c731e0927d2cedd7460c40b30c9eb10e43de
Ashish Kapoor <swiftobjc@gmail.com>
2022-06-07 09:14:52 +0530

    login page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUpPage";
 import LanguageSelector from "./components/LanguageSelector";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <div className="container">
       {window.location.pathname === '/' && <HomePage />}
       {window.location.pathname === '/signup' && <SignUpPage />}
+      {window.location.pathname === '/login' && <LoginPage />}
       <LanguageSelector />
     </div>
   );
